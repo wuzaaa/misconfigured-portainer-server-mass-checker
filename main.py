405fc69e2742d48d4fb7197e6977fad00d5d9cd9
7wuza <65517098+7wuza@users.noreply.github.com>
2022-01-07 17:09:30 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+import requests
+from multiprocessing.pool import ThreadPool
+from functools import partial
+import time
+
+
+# Actual server IP's
+
+# http://185.8.174.140:9000/#!/init/admin
+# http://130.185.76.139:9000/#!/init/admin
+
+
+
+print('''  _____           _        _                    _____ _               _
+ |  __ \         | |      (_)                  / ____| |             | |
+ | |__) |__  _ __| |_ __ _ _ _ __   ___ _ __  | |    | |__   ___  ___| | _____ _ __
+ |  ___/ _ \| '__| __/ _` | | '_ \ / _ \ '__| | |    | '_ \ / _ \/ __| |/ / _ \ '__|
+ | |  | (_) | |  | || (_| | | | | |  __/ |    | |____| | | |  __/ (__|   <  __/ |
+ |_|   \___/|_|   \__\__,_|_|_| |_|\___|_|     \_____|_| |_|\___|\___|_|\_\___|_|
+
+
+
+''')
+
+
+# List of connections failed
+Failed_Connections = []
+
+
+with open('Servers.txt') as f:
+    servers = [line.rstrip() for line in f]
+    f.close()
+
+
+def login(session, server):
+    url = f'http://{server}/api/users/admin/check'
+    try:
+        response = requests.get(url)
+    except:
+        print('[~] Connection to '+server+' failed! Moving on.')
+    if '404' in str(response):
+        with open('Saved.txt', "a") as f:
+                f.write(f'http://'+server+'/#!/init/admin\n')
+        print('\n[!] Portainer server not setup yet, saved URL! '+url+'\n')
+    else:
+        Failed_Connections.append(server)
+        print('[-] Portainer server is setup, moving on.  '+url)
+
+
+
+
+    # url = f'http://{server}/api/settings/public'
+    # response = requests.get(url)
+    #
+    # if """AllowHostNamespaceForRegularUsers': True,""" in str(response.json()):
+    #     print('[!] Portainer server not setup yet, saving6 URL! '+url)
+    #     with open('Saved.txt', "a") as f:
+    #         f.write(url+'\n')
+    #         f.close()
+    #
+    # else:
+    #     print('[-] Portainer server is setup, moving on.  '+url)
+
+
+if __name__ == "__main__":
+    # creating a pool object
+    with ThreadPool(min(len(servers), 5000)) as pool, \
+    requests.Session() as session:
+        # map will return list of None since `login` returns None implicitly:
+        try:
+            pool.map(partial(login, session), servers)
+        except:
+            print('\n\n[*] Job finished!')
+            with open('Saved.txt', "a") as f:
+                f.write('\n')
+                f.close()
